611e1bdab3b851f92faf27278ffe3a6db387caf7
Atnon Shesterikov <shesterikovon@gmail.com>
2024-05-19 13:57:56 +0300

    tg-react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {useEffect} from "react";
+const tg = window.Telegram.WebApp;
+function  App() {
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+    const onClose = () => {
+        tg.close();
+    }
+  return (
+      <div className="App">
+        WORK
+          <button onClick={onClose}>закрыть</button>
+      </div>
+  );
+
+}
+export default App;

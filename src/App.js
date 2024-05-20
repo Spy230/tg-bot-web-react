ec9b1d6ee2ac93c7d54384da7fce4371a135015e
Atnon Shesterikov <shesterikovon@gmail.com>
2024-05-20 13:27:45 +0300

    init/.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import React from 'react';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
+
+
 const tg = window.Telegram.WebApp;
-
-
 
 function  App() {
     const {onToggleButton , tg} = useTelegram();
